1bfe1e27d81b55fbcdb1581f13b7f677e8578c95
MytsV <mytsvictoria@gmail.com>
2023-05-28 00:38:05 +0300

    Implement basic balancing algorithm - Least connections server algorithm - Modify client to send concurrent requests

--- cmd/lb/balancer.go
+++ cmd/lb/balancer.go
@@ -13,20 +13,27 @@ import (
 	"github.com/roman-mazur/design-practice-2-template/signal"
 )
 
+// Some as env variables?
 var (
-	port = flag.Int("port", 8090, "load balancer port")
-	timeoutSec = flag.Int("timeout-sec", 3, "request timeout time in seconds")
-	https = flag.Bool("https", false, "whether backends support HTTPs")
+	port       = flag.Int("port", 8090, "load balancer port")
+	timeoutSec = flag.Int("timeout-sec", 10, "request timeout time in seconds")
+	https      = flag.Bool("https", false, "whether backends support HTTPs")
 
 	traceEnabled = flag.Bool("trace", false, "whether to include tracing information into responses")
 )
 
+// Public?
+type server struct {
+	Url           string
+	ConnectionCnt int
+}
+
 var (
-	timeout = time.Duration(*timeoutSec) * time.Second
-	serversPool = []string{
-		"server1:8080",
-		"server2:8080",
-		"server3:8080",
+	timeout     = time.Duration(*timeoutSec) * time.Second
+	serversPool = []server{
+		{Url: "server1:8080"},
+		{Url: "server2:8080"},
+		{Url: "server3:8080"},
 	}
 )
 
@@ -51,6 +58,7 @@ func health(dst string) bool {
 	return true
 }
 
+// Method on Server?
 func forward(dst string, rw http.ResponseWriter, r *http.Request) error {
 	ctx, _ := context.WithTimeout(r.Context(), timeout)
 	fwdRequest := r.Clone(ctx)
@@ -84,6 +92,17 @@ func forward(dst string, rw http.ResponseWriter, r *http.Request) error {
 	}
 }
 
+// Pass only healthy ones?
+func Balance(pool []server) int {
+	min := 0
+	for i := 1; i < len(pool); i++ {
+		if pool[i].ConnectionCnt < pool[min].ConnectionCnt {
+			min = i
+		}
+	}
+	return min
+}
+
 func main() {
 	flag.Parse()
 
@@ -92,14 +111,17 @@ func main() {
 		server := server
 		go func() {
 			for range time.Tick(10 * time.Second) {
-				log.Println(server, health(server))
+				log.Println(server, health(server.Url))
 			}
 		}()
 	}
 
 	frontend := httptools.CreateServer(*port, http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		// TODO: Рееалізуйте свій алгоритм балансувальника.
-		forward(serversPool[0], rw, r)
+		i := Balance(serversPool)
+		//Lock?
+		serversPool[i].ConnectionCnt++
+		forward(serversPool[i].Url, rw, r)
+		serversPool[i].ConnectionCnt--
 	}))
 
 	log.Println("Starting load balancer...")
