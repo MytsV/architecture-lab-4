f058a2875048257e0daa35261907202714bd29f7
MytsV <mytsvictoria@gmail.com>
2023-05-29 16:21:25 +0300

    Expand debug possibilities for server.go

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -5,27 +5,58 @@ import (
 	"flag"
 	"net/http"
 	"time"
-
+	"sync"
 	"github.com/roman-mazur/design-practice-2-template/httptools"
 	"github.com/roman-mazur/design-practice-2-template/signal"
 )
 
 var port = flag.Int("port", 8080, "server port")
 var delay = flag.Int("delay-sec", 0, "response delay in seconds")
-var healthFailure = flag.Bool("health", false, "whether the server is healthy")
+var healthInit = flag.Bool("health", true, "initial server health")
+var debug = flag.Bool("debug", false, "whether we can change server's health status")
+
+type boolMutex struct {
+	mu sync.Mutex
+	v  bool
+}
+
+func (c *boolMutex) Inverse() {
+	c.mu.Lock()
+	c.v = !c.v
+	c.mu.Unlock()
+}
+
+func (c *boolMutex) Get() bool {
+	c.mu.Lock()
+	res := c.v
+	c.mu.Unlock()
+	return res
+}
+
 
 func main() {
 	flag.Parse()
 	h := new(http.ServeMux)
+	health := boolMutex{v: *healthInit}
+
+	if *debug {
+		h.HandleFunc("/inverse-health", func(rw http.ResponseWriter, r *http.Request) {
+			if r.Method != "POST" {
+				http.Error(rw, "Method not allowed", http.StatusMethodNotAllowed)
+				return
+			}
+			health.Inverse()
+		})
+	}
 
 	h.HandleFunc("/health", func(rw http.ResponseWriter, r *http.Request) {
 		rw.Header().Set("content-type", "text/plain")
-		if *healthFailure {
-			rw.WriteHeader(http.StatusInternalServerError)
-			_, _ = rw.Write([]byte("FAILURE"))
-		} else {
+		if health.Get() {
 			rw.WriteHeader(http.StatusOK)
 			_, _ = rw.Write([]byte("OK"))
+		} else {
+			rw.WriteHeader(http.StatusInternalServerError)
+			_, _ = rw.Write([]byte("FAILURE"))
 		}
 	})
 
